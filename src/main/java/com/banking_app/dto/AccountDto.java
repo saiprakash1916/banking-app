6eeaca28b402428a72738185658a9a2444c8e8b5
sai <saiprakash1916@gmail.com>
2025-04-09 08:41:16 +0530

    Added the new feature for java that is record class

--- src/main/java/com/banking_app/dto/AccountDto.java
+++ src/main/java/com/banking_app/dto/AccountDto.java
@@ -1,12 +1,5 @@
 package com.banking_app.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+public record AccountDto(Long id, String accountHolderName, double balance) {
 
-@Data
-@AllArgsConstructor
-public class AccountDto {
-    private Long id;
-    private String accountHolderName;
-    private double balance;
 }
--- src/main/java/com/banking_app/mapper/AccountMapper.java
+++ src/main/java/com/banking_app/mapper/AccountMapper.java
@@ -6,9 +6,9 @@ import com.banking_app.entity.Account;
 public class AccountMapper {
     public static Account mapToAccount(AccountDto accountDto){
         Account account = new Account(
-                accountDto.getId(),
-                accountDto.getAccountHolderName(),
-                accountDto.getBalance()
+                accountDto.id(),
+                accountDto.accountHolderName(),
+                accountDto.balance()
         );
         return account;
     }
